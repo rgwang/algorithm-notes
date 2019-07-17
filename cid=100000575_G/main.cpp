b1783e8690465d7659a1f8687deaeda8042f307c
rgwang <303698450@qq.com>
2019-07-17 15:37:35 +0800

    cid=100000575_G

---
+++ cid=100000575_G/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void calculate(int a[],int b[])
+{
+
+}
+int main()
+{
+    //cout << "Hello world!" << endl;
+	int N,inNumber[100],;
+	while (scanf("%d",&N)!=EOF)
+    {
+        for (int i=0;i<N;i++)
+        {
+            scanf("%d",&inNumber[i]);
+        }
+    }
+    return 0;
+}
