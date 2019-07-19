ee77d93f346175b32c4de204d396efb3b6ccb478
rgwang <303698450@qq.com>
2019-07-20 00:38:35 +0800

    B1011

---
+++ B1011/main.cpp
@@ -0,0 +1,23 @@
+//A+BºÍC
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+	int T,tcase=1;
+	scanf("%d",&T);
+	while (T--)
+    {
+        long long a,b,c;
+        scanf("%lld%lld%lld",&a,&b,&c);
+        if(a+b>c)
+        {
+            printf("Case #%d: true\n",tcase++);
+        }
+        else {printf("Case #%d: false\n",tcase++);}
+    }
+    return 0;
+}
